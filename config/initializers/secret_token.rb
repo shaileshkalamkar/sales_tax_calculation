dade81609ac71b262535c9ca39f9dfe72359938f
shaileshparamisoft <shailesh@paramisoft.com>
2013-07-26 17:24:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalesTaxCalculation::Application.config.secret_key_base = '7c16bb619a8e7f54ad1f41d58328596e88c08fccbfb63d07509813998f716812c5b2ec8b0ccc722de8be6d01818ddf29745b0112d206a935abe8938473089244'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalesTaxCalculation::Application.config.session_store :cookie_store, key: '_sales_tax_calculation_session'
